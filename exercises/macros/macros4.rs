ecf7675b91ebc1c4a6f536b2fd9d27ae6b68534b
hasdasd12 <113813116+hasdasd12@users.noreply.github.com>
2024-04-24 14:02:53 +0000

    update

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,19 +3,19 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-#[rustfmt::skip]
-macro_rules! my_macro {
-    () => {
-        println!("Check out my macro!");
-    }
-    ($val:expr) => {
-        println!("Look at this other macro: {}", $val);
-    }
+
+macro_rules! my_macro {  
+    () => {  
+        println!("Check out my macro!");  
+    };  
+    ($val:expr) => {  
+        println!("Look at this other macro: {}", $val);  
+    };  
 }
 
 fn main() {
-    my_macro!();
-    my_macro!(7777);
+    my_macro!(); // 无参数调用  
+    my_macro!(7777); // 带一个表达式参数调用
 }
+
