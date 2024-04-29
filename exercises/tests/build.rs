45f2ff20e70df035ea66f2eee096a4b4fb737665
hasdasd12 <113813116+hasdasd12@users.noreply.github.com>
2024-04-29 13:27:12 +0000

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,8 +1,8 @@
 //! This is the build script for both tests7 and tests8.
 //!
 //! You should modify this file to make both exercises pass.
-
-//fn main() {
+use std::env;  
+fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
     // Cargo do it.
@@ -18,10 +18,7 @@
     // Cargo about that.
     //let your_command = "";
     //println!("cargo:{}", your_command);
-//}
-use std::env;  
-  
-fn main() {  
-    
-        println!("cargo:rustc-cfg=pass_feature_enabled");  
-    } 
+    println!("cargo:rustc-cfg=pass_feature_enabled");  
+}
+
+
